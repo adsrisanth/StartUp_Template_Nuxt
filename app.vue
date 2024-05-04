3ea68ca9cc29e0e8ce5ae3ea3cd14db41ecdd2c7
Adari Dinesh Srisanth <adsrisanth2005@gmail.com>
2024-05-05 01:47:01 +0530

    StartUp Template with TailwindCss

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script>
+useHead({
+  title:"Dinesh SriSanth Adari"
+})
+</script>
+
+<template>
+  <div class="bg-gray-950 text-3xl font-merriweather text-white">
+    Welcome to Nuxt
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+import { Title } from "#build/components";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
